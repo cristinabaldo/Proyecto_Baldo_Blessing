e7bf262c92b87a2d52ac973e93c8bae8fef0073d
cristinabaldo <cristina.baldo@correo.unimet.edu.ve>
2025-08-06 10:41:24 -0400

    Obtener departamentos del api a traves del requests

--- API.py
+++ API.py
@@ -1,8 +1,20 @@
 import requests
 import json
-
+from Departamento import Departamento
 
 
 
 def obtener_departamentos():
-    #api_url = "
+    #Obtiene la lista de departamentos del Met Museum
+    api_url = "https://collectionapi.metmuseum.org/public/collection/v1/departments"
+    response = requests.get(api_url)
+
+    #Si la respuesta es exitosa, devuelve la lista de departamentos con sus datos
+    if response.status_code == 200:
+        data = response.json()["departments"]
+
+        departamentos = []
+        for departamento in data:
+            departamentos.append(Departamento(departamento["departmentId"], departamento["displayName"]))
+
+        return departamentos
